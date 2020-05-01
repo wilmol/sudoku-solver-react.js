9c476f4a22cb2f9c758a6b228ceb0aeea8f3362b
Will <willjoemolloy@gmail.com>
2020-05-02 11:27:30 +1200

    Migrate class components to functional components and react hooks (#6)
    Migrate `App` component from class component to functional component.
    
    Replace `setState` calls with `useState` hook.
    
    Closes #2.

--- src/App.js
+++ src/App.js
@@ -1,89 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { board: App.initialBoard() };
-  }
+function initialBoard() {
+  return [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+  ];
+}
 
-  render() {
-    const board = this.state.board;
-    return (
-      <div>
-        <table border="1">
-          <tbody>
-            {board.map((row, i) => {
-              return (
-                <tr key={i}>
-                  {row.map((cell) => (
-                    <td>{cell === 0 ? '' : cell}</td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <button
-          onClick={() => {
-            console.log('Solving...');
-            this.solve().then((b) => (b ? console.log('Solved.') : null));
-          }}
-        >
-          Solve
-        </button>
-        <button
-          onClick={() => {
-            console.log('Resetting...');
-            this.reset();
-            console.log('Reset.');
-          }}
-        >
-          Reset
-        </button>
-      </div>
-    );
+function isValidMove(board, row, col, num) {
+  for (let i = 0; i < 9; i++) {
+    // check row
+    if (board[i][col] === num) {
+      return false;
+    }
+    // check col
+    if (board[row][i] === num) {
+      return false;
+    }
+    // check 3x3 square
+    if (
+      board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + (i % 3)] === num
+    ) {
+      return false;
+    }
   }
+  return true;
+}
 
-  static initialBoard() {
-    return [
-      [5, 3, 0, 0, 7, 0, 0, 0, 0],
-      [6, 0, 0, 1, 9, 5, 0, 0, 0],
-      [0, 9, 8, 0, 0, 0, 0, 6, 0],
-      [8, 0, 0, 0, 6, 0, 0, 0, 3],
-      [4, 0, 0, 8, 0, 3, 0, 0, 1],
-      [7, 0, 0, 0, 2, 0, 0, 0, 6],
-      [0, 6, 0, 0, 0, 0, 2, 8, 0],
-      [0, 0, 0, 4, 1, 9, 0, 0, 5],
-      [0, 0, 0, 0, 8, 0, 0, 7, 9],
-    ];
-  }
+function sleep() {
+  return new Promise((resolve) => setTimeout(resolve, 0));
+}
 
-  reset() {
-    this.setState({ board: App.initialBoard() });
-  }
+function App() {
+  const [board, setBoard] = useState(initialBoard);
 
-  async solve() {
-    await this.sleep(0);
-    const board = this.state.board;
+  async function solve() {
+    await sleep();
+    const boardCopy = [...board];
     for (let row = 0; row < 9; row++) {
       for (let col = 0; col < 9; col++) {
-        if (board[row][col] !== 0) {
+        if (boardCopy[row][col] !== 0) {
           // can't change filled cells
           continue;
         }
         // try 1 to 9 for the empty cell
         for (let num = 1; num <= 9; num++) {
-          if (App.isValidMove(board, row, col, num)) {
+          if (isValidMove(boardCopy, row, col, num)) {
             // valid move, try it
-            board[row][col] = num;
-            this.setState({ board: board });
-            if (await this.solve()) {
+            boardCopy[row][col] = num;
+            setBoard(boardCopy);
+            if (await solve()) {
               return true;
             } else {
               // didn't solve with this move, backtrack
-              board[row][col] = 0;
-              this.setState({ board: board });
+              boardCopy[row][col] = 0;
+              setBoard(boardCopy);
             }
           }
         }
@@ -95,30 +74,40 @@ class App extends Component {
     return true;
   }
 
-  static isValidMove(board, row, col, num) {
-    for (let i = 0; i < 9; i++) {
-      // check row
-      if (board[i][col] === num) {
-        return false;
-      }
-      // check col
-      if (board[row][i] === num) {
-        return false;
-      }
-      // check 3x3 square
-      if (
-        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + (i % 3)] ===
-        num
-      ) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+  return (
+    <div>
+      <table border="1">
+        <tbody>
+          {board.map((row, i) => {
+            return (
+              <tr key={i}>
+                {row.map((cell) => (
+                  <td>{cell === 0 ? '' : cell}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button
+        onClick={() => {
+          console.log('Solving...');
+          solve().then((b) => console.log(b ? 'Solved.' : 'Unsolveable.'));
+        }}
+      >
+        Solve
+      </button>
+      <button
+        onClick={() => {
+          console.log('Resetting...');
+          setBoard(initialBoard);
+          console.log('Reset.');
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default App;
