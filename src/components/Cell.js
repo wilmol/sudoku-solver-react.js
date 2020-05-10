778c044991900f867ebd3e53c73c090afb474b5a
Will Molloy <willjoemolloy@gmail.com>
2020-05-10 14:17:42 +1200

    Implement hover for individual cell
    Colours the cell when hovered. Want to do this for related cells (those
    in same row, column and square).

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Cell = ({ row, col, value, initiallySet }) => {
-  const className = `Cell-row-${row} Cell-col-${col} Cell-initiallySet-${initiallySet} Cell`;
-  return <td className={className}>{value === 0 ? '' : value}</td>;
+  const [isHover, setHover] = useState(false);
+  const className = `Cell-row-${row} Cell-col-${col} Cell-initiallySet-${initiallySet} Cell-isHover-${isHover} Cell`;
+  return (
+    <td
+      onMouseEnter={() => {
+        setHover(true);
+      }}
+      onMouseLeave={() => {
+        setHover(false);
+      }}
+      className={className}
+    >
+      {value === 0 ? '' : value}
+    </td>
+  );
 };
 
 export default Cell;
