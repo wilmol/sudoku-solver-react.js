61f6aefe5a21c77612c7a280d6ab01bf1e9c7858
Will Molloy <willjoemolloy@gmail.com>
2020-05-11 19:31:27 +1200

    Implement hover for neighbouring cells
    Those in same row/col/square. Changes the background colour.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Cell = ({ row, col, value, initiallySet }) => {
-  const [isHover, setHover] = useState(false);
-  const className = `Cell-row-${row} Cell-col-${col} Cell-initiallySet-${initiallySet} Cell-isHover-${isHover} Cell`;
+const Cell = ({ row, col, value, initiallySet, hover, setHover }) => {
+  let className = `Cell Cell-row-${row} Cell-col-${col}`;
+  if (initiallySet) {
+    className += ' Cell-initiallySet';
+  }
+  if (hover.row === row && hover.col === col) {
+    className += ' Cell-hover';
+  } else if (
+    hover.row === row ||
+    hover.col === col ||
+    (Math.floor(hover.row / 3) === Math.floor(row / 3) &&
+      Math.floor(hover.col / 3) === Math.floor(col / 3))
+  ) {
+    className += ' Cell-neighbourHover';
+  }
+
   return (
     <td
-      onMouseEnter={() => {
-        setHover(true);
-      }}
-      onMouseLeave={() => {
-        setHover(false);
-      }}
+      onMouseEnter={() => setHover({ row: row, col: col })}
+      onMouseLeave={() => setHover({ row: -1, col: -1 })}
       className={className}
     >
       {value === 0 ? '' : value}
